218c64e65ce3a606098e06e79ed8ca89b375caeb
kegang <gangke@isoftstone.com>
2023-11-20 22:26:55 +0800

    feat: 新增member会员模块

---
+++ train-member/src/main/java/www/kgstudy/train/member/config/MemberApplication.java
@@ -0,0 +1,14 @@
+package www.kgstudy.train.member.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("www.kgstudy")
+public class MemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberApplication.class, args);
+    }
+}
