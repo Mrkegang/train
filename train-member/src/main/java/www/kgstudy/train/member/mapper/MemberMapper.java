2833cdf264161cd57ca789a714e65a954ea21b23
kegang <gangke@isoftstone.com>
2023-11-28 22:31:43 +0800

    feat: 集成Mybatis官方的代码生成器

--- train-member/src/main/java/www/kgstudy/train/member/mapper/MemberMapper.java
+++ train-member/src/main/java/www/kgstudy/train/member/mapper/MemberMapper.java
@@ -1,6 +1,30 @@
 package www.kgstudy.train.member.mapper;
 
-public interface MemberMapper {
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+import www.kgstudy.train.member.domain.Member;
+import www.kgstudy.train.member.domain.MemberExample;
 
-    int count();
+public interface MemberMapper {
+    long countByExample(MemberExample example);
+
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
 }
